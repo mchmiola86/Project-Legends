a5f49d8e281e4a01c9ab81614c995b48bfe9fa0f
Michael Chmiola <116197197+mchmiola86@users.noreply.github.com>
2023-11-15 08:49:42 -0500

    Update and rename script.js to academics.js

---
+++ academics.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<body style="background-color:#520B00">
+    <div align="center">
+<img src="cymday.jpg" style="width:200px;height:200px;">
+   <div class="container">
+     
+<font size="5">
+  <div align="Center">
+    <div button
+  float: left;
+  margin: 0;
+  </form></div>
+<form class="form-horizontal" action="index.html">
+<button>Home</button>
+<button formaction="AP.html">AP Courses</button>
+</form>
+
+</div>
+<P></P>
+<h style="color:#FFFFFF; font-family:Cambria"> Academics </h>
+</font>
+<p></p>
+<p style="color:#FFFFFF; font-family:Cambria">Here at Hutch Tech, we pride ourselves on our academic achievements. Our expectations for all students are exceedingly high.We have several available AP courses to choose from, whether it be AP Calculus or AP Art.</p>
+<p style="color:#520B00">.</p>
+<p style="color:#520B00">.</p>
+<p style="color:#520B00">.</p>
+<p style="color:#520B00">.</p>
+<p style="color:#FFFFFF; font-family:Cambria">Feel free to try out our grade calculator below!</p>
+<p></p>
+<iframe src="https://trinket.io/embed/python/0569e35696?outputOnly=true&runOption=run" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
